be186cda8bcf2b4a97b737cbd6bddd8938eba160
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-07-12 13:26:27 +0800

    Update Label.m

--- Label.m
+++ Label.m
@@ -14,13 +14,16 @@ SNR = 6;
 CNR = 15;
 H = 4;
 %
-% para.mat 檔案 存有 H SNR N M 的資料 從 RD_map_dataset.m 或 RD_map_dataset_clutter.m 產生
+% para.mat 檔案 存有 H SNR N M 的資料 從 RD_map_dataset.m / RD_map_dataset_clutter.m 產生
+% 但會覆寫掉上面 H SNR 設定 且 RD_map_dataset.m / RD_map_dataset_clutter.m 的 SNR 設定是像
+% 0:5:10 跟 SNR = 某個定值 如6 衝突? 
 load para.mat 
 %
-% fid=fopen(['D:\YU\my_yolo3\','2007_train.txt'],'a'); %寫入檔案路徑(用"a"時，如果文字中已經存在資料，不會清空資料，而是在資料之後寫入，而"w"會清空原本的資料，重新寫入)
+% fid=fopen(['D:\YU\my_yolo3\','2007_train.txt'],'a'); % 寫入檔案路徑(用"a"時，如果文字中已經存在資料，不會清空資料，而是在資料之後寫入，而"w"會清空原本的資料，重新寫入)
 % fid=fopen(['D:\YU\my_yolo3\','2007_val.txt'],'a');
 %
 % 沒有 RD_map_label 的資料
+% size(RD_map_label) = [N M length(SNR], e.g. 
 for i = 1:size(RD_map_label, 3)
     [nn, mm] = find(RD_map_label(:, :, i) == 1); % target的位置
     for ii = 1:H
