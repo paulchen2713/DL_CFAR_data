3164a7dd2142e1ae70988d87e7e4af507dade5ff
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-08-06 12:26:22 +0800

    Update save_csv.m

--- save_csv.m
+++ save_csv.m
@@ -1,5 +1,5 @@
 %
-% generate train.csv, valid.csv and test.csv
+% generate train.csv and test.csv
 %
 fid1 = fopen(['D:\Datasets\RD_maps\', 'train.csv'], 'a');
 fid2 = fopen(['D:\Datasets\RD_maps\', 'test.csv'], 'a');
@@ -7,10 +7,14 @@ max_iter = 1600; % total number
 num_test = 100;
 num_train = max_iter - num_test;
 for iter = 1:num_train
-    fprintf(fid1,'%d.jpg,%d.txt\n', iter, iter);
+    % fprintf(fid1,'%d.png,%d.txt\n', iter, iter); % 16-by-16 gray scale images
+    fprintf(fid1,'%d_sc.jpg,%d.txt\n', iter, iter); % 416-bi-416 scaled color images
 end
+fprintf('%d\n', num_train);
 for iter = num_train + 1:max_iter
-    fprintf(fid2,'%d.jpg,%d.txt\n', iter, iter);
+    % fprintf(fid2,'%d.png,%d.txt\n', iter, iter); % 16-by-16 gray scale images
+    fprintf(fid2,'%d_sc.jpg,%d.txt\n', iter, iter); % 416-bi-416 scaled color images
 end
+fprintf('%d\n', max_iter);
 fclose(fid1);
 fclose(fid2);
